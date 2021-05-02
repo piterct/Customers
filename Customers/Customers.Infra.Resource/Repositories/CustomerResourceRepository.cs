f73cee54106bde5da0f6ac3691c03c323711af2d
Michael Peter <michael_piterct@hotmail.com>
2021-05-02 03:03:10 -0300

    Customer Resource Repository

---
+++ Customers/Customers.Infra.Resource/Repositories/CustomerResourceRepository.cs
@@ -0,0 +1,38 @@
+﻿using Customers.Domain.Commands.Customer;
+using Customers.Domain.Commands.Inputs;
+using Customers.Domain.Entities;
+using Customers.Domain.Queries;
+using Customers.Infra.Resource.Resource.Customer;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customers.Infra.Json.Repositories
+{
+    public class CustomerResourceRepository 
+    {
+        public async ValueTask<CustomerEntity> GetCustomerById(GetCustomerByIdCommandInput command )
+        {
+            var customers = JsonConvert.DeserializeObject<CustomerQuery>(CustomerResource.Customers);
+
+            return await Task.FromResult(customers.Clientes.Where(x => x.Id == command.IdCustomer).FirstOrDefault());
+        }
+
+        public async ValueTask<CustomerEntity> GetCustomerByCpf(GetCustomerByCPFCommandInput command)
+        {
+            var customers = JsonConvert.DeserializeObject<CustomerQuery>(CustomerResource.Customers);
+
+            return await Task.FromResult(customers.Clientes.Where(x => x.Cpf == command.CPF).FirstOrDefault());
+        }
+
+        public async ValueTask<List<CustomerEntity>> SortCustomersByName()
+        {
+            var customers = JsonConvert.DeserializeObject<CustomerQuery>(CustomerResource.Customers);
+
+            return await Task.FromResult(customers.Clientes);
+        }
+
+    }
+}
+
