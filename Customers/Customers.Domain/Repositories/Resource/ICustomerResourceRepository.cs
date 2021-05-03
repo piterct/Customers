1c2165ffab8f96f280bf27b7e78acee33082619a
Michael Peter Muniz da Silva <michael.peter@consigamais.com.br>
2021-05-03 09:42:05 -0300

    ICustomerResourceRepository

---
+++ Customers/Customers.Domain/Repositories/Resource/ICustomerResourceRepository.cs
@@ -0,0 +1,15 @@
+﻿using Customers.Domain.Commands.Customer;
+using Customers.Domain.Commands.Inputs;
+using Customers.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Customers.Domain.Repositories.Resource
+{
+    public interface ICustomerResourceRepository
+    {
+        ValueTask<List<CustomerEntity>> SortCustomersByName();
+        ValueTask<CustomerEntity> GetCustomerById(GetCustomerByIdCommandInput command);
+        ValueTask<CustomerEntity> GetCustomerByCpf(GetCustomerByCPFCommandInput command);
+    }
+}
