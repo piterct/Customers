59e85095687b059d530f4114dd51987db2ea517c
Michael Peter <michael_piterct@hotmail.com>
2021-07-13 20:34:00 -0300

    SuspendProcessInMemory

---
+++ Customers/Customers.Domain/Helpers/MemoryHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Customers.Domain.Helpers
+{
+    public static class MemoryHelper
+    {
+        public static void SuspendProcessInMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
