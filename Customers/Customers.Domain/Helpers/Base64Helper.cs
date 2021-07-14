e9ae99030c1235e4c10eebda18b05eb8668f3774
Michael Peter <michael_piterct@hotmail.com>
2021-07-14 00:41:55 -0300

    Base64Helper

---
+++ Customers/Customers.Domain/Helpers/Base64Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Customers.Domain.Helpers
+{
+    public static class Base64Helper
+    {
+        public static string ByteToBase64(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
