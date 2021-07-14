69bdb03530425126e16fda8b8642c566b59a9f31
Michael Peter <michael_piterct@hotmail.com>
2021-07-14 00:39:28 -0300

    StreamHelper

---
+++ Customers/Customers.Domain/Helpers/StreamHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace Customers.Domain.Helpers
+{
+    public static class StreamHelper
+    {
+        public static StreamContent Base64ToStream(string base64)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            return new StreamContent(new MemoryStream(bytes));
+        }
+
+        public static Stream ByteToStream(byte[] contentByte)
+        {
+            return new MemoryStream(contentByte);
+        }
+    }
+}
