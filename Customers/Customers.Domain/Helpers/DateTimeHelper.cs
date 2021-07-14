2a81b21ea2a32ade24cbf40cabec35dbc03136ce
Michael Peter <michael_piterct@hotmail.com>
2021-07-14 00:40:25 -0300

    GetLastDayOfMonth

---
+++ Customers/Customers.Domain/Helpers/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Customers.Domain.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime GetLastDayOfMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, DateTime.DaysInMonth(dateTime.Year, dateTime.Month));
+        }
+    }
+}
